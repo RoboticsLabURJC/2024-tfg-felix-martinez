7b0f98168b982a5f117a30914479ae8598b4b506
Félix <felixmaral131@gmail.com>
2024-11-02 20:39:56 +0100

    Updating Post Semana 6 y 7

--- segmentation/planar _segmentation.py
+++ segmentation/planar _segmentation.py
@@ -14,14 +14,6 @@ nube_puntos = cargar_nube_bin("/home/felix/Escritorio/TFG/datasets/Goose/goose_3
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(nube_puntos)
 
-# Filtrar por altura (opcional)
-# Filtramos para incluir solo puntos con valores z bajos (asumiendo que el suelo es más bajo) RANSAC
-z_limite = -1.5  # Ajusta este valor según la altura esperada del suelo en tu dataset
-puntos_suelo = np.asarray(pcd.points)
-puntos_suelo = puntos_suelo[puntos_suelo[:, 2] < z_limite]
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(puntos_suelo)
-
 # Segmentar el plano del suelo
 plane_model, inliers = pcd.segment_plane(distance_threshold=0.2, ransac_n=3, num_iterations=1000)
 [a, b, c, d] = plane_model
@@ -35,7 +27,19 @@ otros_cloud = pcd.select_by_index(inliers, invert=True)
 
 # Colorear el plano del suelo y otros puntos para visualización
 suelo_cloud.paint_uniform_color([1, 0, 0])
-otros_cloud.paint_uniform_color([1, 1, 1])
+otros_cloud.paint_uniform_color([0, 0, 1])
 
-# Visualizar el resultado
-o3d.visualization.draw_geometries([suelo_cloud, otros_cloud], window_name="Segmentación del Suelo")
+# Configuración de visualización: ajustar el tamaño de los puntos
+vis = o3d.visualization.Visualizer()
+vis.create_window(window_name="Segmentación del Suelo")
+vis.add_geometry(suelo_cloud)
+vis.add_geometry(otros_cloud)
+
+# Modificar el tamaño de los puntos
+render_option = vis.get_render_option()
+render_option.point_size = 1.5  # Ajusta el tamaño de los puntos aquí
+
+# Ejecutar la visualización
+vis.run()
+vis.destroy_window()
+

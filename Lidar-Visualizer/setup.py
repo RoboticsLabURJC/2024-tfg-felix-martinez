54a30d4e703c9edbf7a28e5cbb89d3288c52b8de
Félix <felixmaral131@gmail.com>
2024-11-06 13:41:47 +0100

    Updating setup

--- Lidar-Visualizer/setup.py
+++ Lidar-Visualizer/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="lidar_visualizer",
     version="0.2",
-    packages=find_packages(where="src"),  # Busca los paquetes en la carpeta 'src'
-    package_dir={"": "src"},              # Indica que el directorio base para paquetes es 'src'
+    packages=find_packages(where="src"),  
+    package_dir={"": "src"},              
     install_requires=[
         "open3d",
         "numpy",
@@ -12,14 +12,14 @@ setup(
         "plyfile",
         "pygame",
         "carla",
-        "tk",  # En algunos casos, `tk` puede no instalarse, ya que suele venir con Python
+        "tk",  
     ],
     entry_points={
         "console_scripts": [
-            "lidar-viz=main:main",  # Crea un comando 'lidar-viz' para ejecutar el programa
+            "lidar-viz=main:main",  
         ],
     },
-    include_package_data=True,  # Incluye datos adicionales, si los hay
+    include_package_data=True,  
     description="Proyecto de visualización de datos LiDAR con soporte para archivos y simulador CARLA",
     author="Félix Martínez",
     author_email="felixmaral131@gmail.com",

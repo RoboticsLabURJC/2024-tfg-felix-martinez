0294c9fd8a1631d59ec5b7de5830e098136ea095
Félix <felixmaral131@gmail.com>
2024-11-08 18:33:16 +0100

    Updating planar_segmentation

--- segmentation/planar _segmentation.py
+++ segmentation/planar _segmentation.py
@@ -11,7 +11,7 @@ def visualize(vis, geometries):
     for geom in geometries:
         vis.add_geometry(geom)  # Agrega cada geometría de forma individual
     opt = vis.get_render_option()
-    opt.point_size = 1.4
+    opt.point_size = 1.6
     vis.run()
     vis.destroy_window()
 
@@ -22,8 +22,8 @@ def calcular_angulo(v1, v2):
     return np.arccos(np.clip(np.dot(v1, v2), -1.0, 1.0)) * (180.0 / np.pi)
 
 # Cargar los datos de la nube de puntos
-data = cargar_nube_bin("/home/felix/Escritorio/TFG/datasets/Goose/goose_3d_val/lidar/val/2023-05-15_neubiberg_rain/2023-05-15_neubiberg_rain__0682_1684158172209467336_vls128.bin")
-
+data = cargar_nube_bin("/home/felix/Escritorio/TFG/datasets/Goose/goose_3d_val/lidar/val/2023-05-17_neubiberg_sunny/2023-05-17_neubiberg_sunny__0426_1684329911503929495_vls128.bin")
+print(len(data))
 
 # Crear la nube de puntos de Open3D
 point_cloud = o3d.geometry.PointCloud()
@@ -42,15 +42,15 @@ point_cloud.estimate_normals(
 
 # Detectar parches planos en la nube de puntos
 plane_patches = point_cloud.detect_planar_patches(
-    normal_variance_threshold_deg=40.0, # Limite de la variacion permitida entre las normales de los puntos de una region (Grados). Cuanto más pequeño, más estricto
-    coplanarity_deg=1.0, # Establece la distribucion de la distancia entre los puntos que forman el plano. Cuanto mas pequeño, mas estricto. (grados)
-    outlier_ratio=0.85, # Porcentaje maximo de puntos rechazados en la región para rechazar el plano estimado
-    min_plane_edge_length=0.0, # Distancia minima de la arista mas larga del plano (filtrar planos pequeños)
+    normal_variance_threshold_deg=30.0, # Limite de la variacion permitida entre las normales de los puntos de una region (Grados). Cuanto más pequeño, más estricto
+    coplanarity_deg=20, # Establece la distribucion de la distancia entre los puntos que forman el plano. Cuanto mas pequeño, mas estricto. (grados)
+    outlier_ratio=1.0, # Porcentaje maximo de puntos rechazados en la región para rechazar el plano estimado
+    min_plane_edge_length=0.1, # Distancia minima de la arista mas larga del plano (filtrar planos pequeños)
     min_num_points=0, # Cantidad minima de puntos para que se acepte el plano en la region
-    search_param=o3d.geometry.KDTreeSearchParamKNN(knn=60) # Define el parametro de vecinos mas cercanos para la realizacion de los otros algoritmos
+    search_param=o3d.geometry.KDTreeSearchParamKNN(knn=30) # Define el parametro de vecinos mas cercanos para la realizacion de los otros algoritmos
 )
 
-vertical_tolerance = 40
+vertical_tolerance = 20
 # Crear meshes de los parches planos para visualizarlos
 meshes = []
 for patch in plane_patches:
@@ -63,7 +63,7 @@ for patch in plane_patches:
         mesh = o3d.geometry.TriangleMesh.create_from_oriented_bounding_box(
             patch, scale=[1, 1, 0.0001]  # Escala para aplanar los parches
         )
-        mesh.paint_uniform_color([0, 1, 0])  # Color verde para los parches
+        mesh.paint_uniform_color([1, 0, 0])  # Color rojo para los parches
         meshes.append(mesh)
 
 # Visualizar los meshes junto con la nube de puntos
